2ef2fc9c73a91d38472e68b6dd9048f045ac016e
Blaise <blaise.kal@paylogic.eu>
2019-04-02 17:26:25 +0200

    Support kwargs in Schema.deserialize

--- tests/deserialize/test_deserialize.py
+++ tests/deserialize/test_deserialize.py
@@ -19,3 +19,58 @@ def test_attr_decorator_setter():
     output = {}
     Schema.deserialize({"total": 555}, output=output)
     assert output["total"] == 321
+
+
+def test_deserialize_kwargs():
+    """Test that context is accessible in all attr variations"""
+
+    class Schema(halogen.Schema):
+        @halogen.attr()
+        def total_decorator(obj, custom_kwarg):
+            return obj['total'][custom_kwarg]
+
+        total_lambda = halogen.Attr(attr=lambda obj, custom_kwarg: obj['total'][custom_kwarg])
+
+        total_nested = halogen.Attr(
+            halogen.Schema(nested=halogen.Attr(attr='mykey')),
+            attr='total',
+        )
+
+        total_dot_sep_str = halogen.Attr(attr='total.mykey')
+
+    deserialized = Schema.deserialize(
+        {"total": {"mykey": 123}},
+        custom_kwarg='mykey'
+    )
+
+    assert deserialized["total_decorator"] == 123
+    assert deserialized["total_lambda"] == 123
+    assert deserialized["total_nested"]["nested"] == 123
+    assert deserialized["total_dot_sep_str"] == 123
+
+
+def test_deserialize_kwargs_list():
+    """Test that context is maintained across a list"""
+
+    class Schema(halogen.Schema):
+        @halogen.attr()
+        def total(obj, custom_kwarg):
+            return obj['total'][custom_kwarg]
+
+    class ListSchema(halogen.Schema):
+        deserialized_list = halogen.attr(halogen.types.List(Schema), attr='mylist')
+
+    deserialized = ListSchema.deserialize(
+        {
+            "mylist": [
+                {"total": {"mykey": 123}},
+                {"total": {"mykey": 234}},
+            ]
+        },
+        custom_kwarg='mykey'
+    )
+
+    assert deserialized['deserialized_list'] == [
+        {"total": 123},
+        {"total": 234},
+    ]
