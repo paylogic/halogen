87810172ad2b51ac947ece2bf3bb29491809aefe
Ruben Kip <ruben.kip@paylogic.eu>
2024-09-19 16:32:33 +0200

    more python version support

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     packages=["halogen", "halogen.vnd"],
     install_requires=["cached-property", "isodate", "python-dateutil", "pytz", "six"],
