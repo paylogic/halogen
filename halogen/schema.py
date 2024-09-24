bf6038f9fdc717fcda6471aa019d88157bd698c2
Ruben Kip <ruben.kip@paylogic.eu>
2024-09-24 16:24:10 +0200

    Update Accessor

--- halogen/schema.py
+++ halogen/schema.py
@@ -75,6 +75,10 @@ class Accessor(object):
         assert isinstance(self.getter, string_types), "Accessor must be a function or a dot-separated string."
 
         for attr in self.getter.split("."):
+            if obj is None:
+                # If obj ever becomes None do not try access attribute
+                return None
+            
             if isinstance(obj, dict):
                 obj = obj[attr]
             else:
